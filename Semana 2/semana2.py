fa5fb7042f39e37e2416a228e2c5336768c30b65
ana.cvsilva <ana.cvsilva@ufpe.br>
2020-06-21 09:07:02 -0300

    correcao da funcao mediana na inputacao de dados

--- Semana 2/semana2.py
+++ Semana 2/semana2.py
@@ -54,7 +54,7 @@ def main():
             st.subheader('Dados Inputados faça download abaixo : ')
             st.markdown(get_table_download_link(df_inputado), unsafe_allow_html=True)
         if select_method == 'Mediana':
-            df_inputado = df[lista_colunas].fillna(df[lista_colunas].mean())
+            df_inputado = df[lista_colunas].fillna(df[lista_colunas].median())
             exploracao_inputado = pd.DataFrame({'nomes': df_inputado.columns, 'tipos': df_inputado.dtypes, 'NA #': df_inputado.isna().sum(),
                                        'NA %': (df_inputado.isna().sum() / df_inputado.shape[0]) * 100})
             st.table(exploracao_inputado[exploracao_inputado['tipos'] != 'object']['NA %'])
